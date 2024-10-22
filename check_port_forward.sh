41b1785217b03c9a441f3601a15d3cadc82e4513
kamegoro <tentyo0620@icloud.com>
2024-10-22 23:43:33 +0900

    fix: shell

--- check_port_forward.sh
+++ check_port_forward.sh
@@ -3,9 +3,10 @@
 PORT=10080
 HOST=localhost
 
-if ns -z $HOST $PORT; then
-  echo "Port $PORT is open on $HOST"
-else
-  echo "port $PORT is not open on $HOST"
+if lsof -i :$PORT; then
+  echo "Port $PORT is already in use on $HOST. Skipping port-forward."
   exit 1
+else
+  echo "Port $PORT is not in use on $HOST. Proceeding with port-forward."
+  exit 0
 fi
